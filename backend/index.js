63944ddea933d6c4d3df93c0e8212805b03d1380
unknown <admin@DESKTOP-DOUF99V.epiccorporations.com>
2024-10-01 11:38:00 +0530

    backend change on index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,6 @@ const express = require("express")
 const app = express();
 const port= 5000;
 app.listen(port,()=>{
-console.log(`server is running on Port ${port}`);    
+console.log(`server is running on Port  is ${port}`);    
 })
 // console.log(app.listen());
